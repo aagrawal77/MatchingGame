ecd8fc3f1c54d9141f156c83888af5f1506ca257
Aarushi <aarushi.agrawal77@gmail.com>
2019-09-12 19:02:31 -0700

    started proj

--- app/src/main/java/com/example/matchinggame/MainActivity.java
+++ app/src/main/java/com/example/matchinggame/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.matchinggame;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button start;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        start = findViewById(R.id.startBtn);
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
